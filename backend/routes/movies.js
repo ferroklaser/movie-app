d2e6659acf2c1a382897c277aa3a0340488dd7cc
Win Paing Oo <winpaing000@gmail.com>
2025-12-04 21:28:51 +0800

    Add post and delete

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -14,4 +14,29 @@ router.get('/', async (req, res) => {
     }
 });
 
+//Insert movie
+router.post('/', async (req, res) => {
+    try {
+        const { api_id, title, poster_url, rating, release_date } = req.body;
+        const result = await pool.query(
+            'INSERT INTO movies (api_id, title, poster_url, rating, release_date) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+            [api_id, title, poster_url, rating, release_date]);
+        //response back to front end so it knows to update
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//Delete a movie
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await pool.query('DELETE FROM movies WHERE id = $1', [id]);
+        res.json('Movie deleted successfully');
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
 export default router;
