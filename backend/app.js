05c24145205464bd27bdd8e51519cbbf6604336b
Win Paing Oo <winpaing000@gmail.com>
2025-12-16 10:44:49 +0800

    Add CORS

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 import moviesRouter from './routes/movies.js'
 import express from 'express'
+import cors from 'cors';
 
 const app = express();
 const port = 3000
 
-app.use(express.json())
+
+app.use(express.json());
+app.use(cors());
 app.use('/movies', moviesRouter);
+app.use(cors({ origin: 'http://localhost:3001' }));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -25,7 +25,7 @@ router.post('/', async (req, res) => {
         res.status(200).json(result.rows[0]);
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Error inserting movie');
+        res.status(500).json('Error inserting movie');
     }
 });
 
